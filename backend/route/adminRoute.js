ccfb6d32397abd50e3a7578936a5cd8f99cda5cf
Mayank Pandey <mayankp2k1@gmail.com>
2025-05-17 00:37:15 +0530

    new push

--- backend/route/adminRoute.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const route=express.Router();
-const {login, signup}=require('../controller/adminController')
-route.post('/signup',signup)
-route.post('/login',login)
-
-module.exports=route
